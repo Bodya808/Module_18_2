5922330e9bc2ccc5f2d959c306d369ac8d661583
Bogdan <paradise2662@gmail.com>
2024-12-12 13:35:44 +0300

    Add files via upload

---
+++ UrbanDjango/UrbanDjango/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from task2.views import func_view, ClassView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('class/', ClassView.as_view()),
+    path('', func_view)
+]
