5e077c0db835a69d2724623054bafbe4124c47bf
Bogdan <paradise2662@gmail.com>
2024-12-13 15:10:12 +0300

    Add files via upload

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from task2.views import func_view, ClassView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('class/', ClassView.as_view()),
-    path('', func_view)
+    path('', func_view),  # Корневой маршрут
+    path('functional/', func_view),  # Новый маршрут для /functional/
 ]
